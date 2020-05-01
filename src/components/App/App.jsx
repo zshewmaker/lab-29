c627febc01c57283a07c8f899b61579716844221
introcosm <happysimulacrum@protonmail.com>
2020-05-01 16:00:11 -0700

    Structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import Header from './Header.jsx';
+import Response from './Response.jsx';
+import Request from './Request.jsx';
 
 export default function App() {
-  return <h1>Hello World</h1>;
-}
-  
+  return (
+    <div>
+      <Header></Header>
+      <Request></Request>
+      <Response></Response>
+    </div> 
+  )
+  ;
+} 
