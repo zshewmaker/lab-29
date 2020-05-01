bd60c3007f93fac3433a44279e829d8d64bfd3f9
introcosm <happysimulacrum@protonmail.com>
2020-05-01 16:19:39 -0700

    Basic

--- src/components/App/Request.jsx
+++ src/components/App/Request.jsx
@@ -3,7 +3,14 @@ import PropTypes from 'prop-types';
 
 const Request = () => (
   <form>
-      Request
+    URL:<input type="text"/>
+    Method:
+    <input type="radio" name="method"/> get
+    <input type="radio" name="method"/> post
+    <input type="radio" name="method"/> put
+    <input type="radio" name="method"/> delete
+    JSON: <textarea></textarea>
+    <button>Go!</button>
   </form>
 );
 
